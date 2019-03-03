0ce54a0edd74def4dcab7a60e67acb4a0f67c21c
Devabhi <devabhixda@gmail.com>
2019-03-03 13:51:11 +0530

    Implement ON/OFF and Disconnect buttons
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- app/src/main/java/ml/systern/automation/MainActivity.java
+++ app/src/main/java/ml/systern/automation/MainActivity.java
@@ -18,15 +18,18 @@ import java.io.UnsupportedEncodingException;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1 = (Button) findViewById(R.id.btn1);
-                String clientId = MqttClient.generateClientId();
-                MqttAndroidClient client =
-                        new MqttAndroidClient(MainActivity.this, "tcp://broker.hivemq.com:1883",
-                                clientId);
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(MainActivity.this, "tcp://systern.ml:1883",
+                        clientId);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,6 +47,59 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                 } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    IMqttToken discontoken = client.disconnect();
+                    discontoken.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            Toast.makeText(MainActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        }
+                    });
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String topic = "/test/light1";
+                String payload = "1";
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String topic = "/test/light1";
+                String payload = "0";
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
                     e.printStackTrace();
                 }
             }
